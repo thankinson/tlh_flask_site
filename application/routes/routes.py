6231d64b0967f6f31af5a65a7c7a390a98f312f3
tom <t_hankinson@outlook.com>
2022-09-12 17:35:51 +0100

    small update

--- application/routes/routes.py
+++ application/routes/routes.py
@@ -17,25 +17,6 @@ def register():
             Userservice.Adduser(form=form)
             message = "User added to database"
         except:
-            message = "user already in db"
+            message = "User Name or Email Already in use"
 
     return render_template('signup.html', form=form, message=message)
-
-
-################################################################
-####           functions that have been replaced            ####
-    # if form.validate_on_submit():
-    #     hash_pw = bcrypt.generate_password_hash(form.password.data)
-    #     users = Users(
-    #         user_name = form.user_name.data,
-    #         first_name = form.first_name.data,
-    #         last_name = form.last_name.data,
-    #         user_email = form.user_email.data,
-    #         password = hash_pw
-    #     )
-
-    #     db.session.add(users)
-    #     db.session.commit()
-    #     message = "User added to database"
-
-    # return render_template('signup.html', form=form, message=message)
