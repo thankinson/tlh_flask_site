4ac9c93fa7951f30f85c3403566cee49b83a5273
Tom <t_hankinson@outlook.com>
2022-09-15 09:17:44 +0100

    Moved login form to signup page. falls over now when you signup or create account

--- application/routes/routes.py
+++ application/routes/routes.py
@@ -8,24 +8,11 @@ from flask_login import current_user
 @app.route('/')
 @app.route('/home')
 def index():
-    message = ""
-    Loginservice.is_logged_in()
-    form = UserLogin()
-    if form.validate_on_submit():
-        try:
-            Loginservice.log_in(form=form)
-            if current_user.is_authenticated:
-                # return redirect(url_for('index'))
-                message = "Success... Your in!"
-            else:
-                message = "User Name or Password Incorrect"
-        except:
-            message = "Fatel Error: The Admin Gods do not smile upon you" 
-    return render_template('index.html', form=form, message=message)
+    return render_template('index.html')
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    # Loginservice.is_logged_in()
+    Loginservice.is_logged_in()
     message = ""
     form = UserRegistration()
     if form.validate_on_submit():
@@ -34,23 +21,35 @@ def register():
             message = "User added to database"
         except:
             message = "User Name or Email Already in use"
-
-    return render_template('signup.html', form=form, message=message)
-
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    print("Login page hit")
-    message = ""
-    Loginservice.is_logged_in()
-    form = UserLogin()
-    if form.validate_on_submit():
+    
+    # login section on signup
+    logform = UserLogin()
+    if logform.validate_on_submit():
         try:
-            Loginservice.log_in(form=form)
+            Loginservice.log_in(logform=logform)
             if current_user.is_authenticated:
                 return redirect(url_for('index'))
             else:
                 message = "User Name or Password Incorrect"
         except:
             message = "Fatel Error: The Admin Gods do not smile upon you"  
-    return render_template('login.html', form=form, message=message)
+
+    return render_template('signup.html', form=form, logform=logform, message=message)
+
+
+# @app.route('/login', methods=['GET', 'POST'])
+# def login():
+#     print("Login page hit")
+#     message = ""
+#     Loginservice.is_logged_in()
+#     form = UserLogin()
+#     if form.validate_on_submit():
+#         try:
+#             Loginservice.log_in(form=form)
+#             if current_user.is_authenticated:
+#                 return redirect(url_for('index'))
+#             else:
+#                 message = "User Name or Password Incorrect"
+#         except:
+#             message = "Fatel Error: The Admin Gods do not smile upon you"  
+#     return render_template('login.html', form=form, message=message)
