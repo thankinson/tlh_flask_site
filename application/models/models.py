73212e48db4ddb2ee28a57390ee7eab3998abdca
tom <t_hankinson@outlook.com>
2022-09-19 16:47:43 +0100

    Builds DB  only if i edit out the admintable lines

--- application/models/models.py
+++ application/models/models.py
@@ -9,14 +9,16 @@ class Users(db.Model, UserMixin):
     user_email = db.Column(db.String(30), nullable=False, unique=True)
     password = db.Column(db.String(200), nullable=False)
     remember_user = db.Column(db.Boolean, default=False)
+    admin_id = db.relationship('UserAdmin', backref='users')
+
     
-    def __repr__(self) -> str:
-        return ''.join(['UserId: ', str(self.id), '\r\n',
-        'Email: ', self.user_email], '\r\n'
-        'Name: ', self.first_name, ' ', self.last_name,
-        '\r\n'
-        'User Name: ', self.user_name 
-        )
+    # def __repr__(self) -> str:
+    #     return ''.join(['UserId: ', str(self.id), '\r\n',
+    #     'Email: ', self.user_email], '\r\n'
+    #     'Name: ', self.first_name, ' ', self.last_name,
+    #     '\r\n'
+    #     'User Name: ', self.user_name 
+    #     )
 
     def is_active(self):
         return True
@@ -31,3 +33,20 @@ class Users(db.Model, UserMixin):
 @login_manager.user_loader
 def load_user(id):
     return Users.query.get(int(id))
+
+class AdminTable(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_name = db.Column(db.String(30), nullable=False, unique=True)
+    first_name = db.Column(db.String(30), nullable=False)
+    last_name = db.Column(db.String(30), nullable=False)
+    user_id = db.relationship('UserAdmin', backref='admintable')
+
+class UserAdmin(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    admin_id = db.Column('admin_id', db.Integer, db.ForeignKey('admintable.id') )
+    user_id = db.Column('user_id', db.Integer, db.ForeignKey('users.id') )
+
+    
+
+
+
--- create.py
+++ create.py
@@ -1,9 +1,35 @@
 from application import db
-from application.models.models import Users
+from application.models.models import Users, AdminTable, UserAdmin
+from application import bcrypt
 
 # check_user = Users.query.filter_by(user_name='thankinson'.lower()).first()
 
 # print(check_user.user_name)
 
+
+
+
 db.drop_all()
 db.create_all()
+# add usr works
+# hash_pw = bcrypt.generate_password_hash('thankinson')
+# AdminUser = Users(
+#     user_name='thankinson',
+#     first_name='tom',
+#     last_name='hankinson',
+#     user_email='tom_lh@live.co.uk',
+#     password= hash_pw
+#     )
+# db.session.add(AdminUser)
+
+# addAdmin = AdminTable(
+#     user_name='thankinson',
+#     first_name='tom',
+#     last_name='hankinson'
+#     )
+# db.session.add(addAdmin)
+
+# addUserAdmin = UserAdmin(users=AdminUser, admintable=addAdmin)
+# db.session.add(addUserAdmin)
+
+# db.session.commit()
