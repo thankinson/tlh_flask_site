6b75a5b2994aeb90298a2e56294c0c4f1f544789
Tom <t_hankinson@outlook.com>
2022-10-04 15:14:15 +0100

    update

--- db/CreateTable.sql
+++ db/CreateTable.sql
@@ -1,3 +1,11 @@
+CREATE TABLE IF NOT EXISTS user_admin (
+	id INTEGER NOT NULL, 
+	user_id INTEGER, 
+	roles_id INTEGER, 
+	PRIMARY KEY (id), 
+	FOREIGN KEY(user_id) REFERENCES users (id)
+);
+
 CREATE TABLE IF NOT EXISTS users (
 	id INTEGER NOT NULL, 
 	user_name VARCHAR(30) NOT NULL, 
@@ -9,18 +17,11 @@ CREATE TABLE IF NOT EXISTS users (
 	PRIMARY KEY (id), 
 	UNIQUE (user_name), 
 	UNIQUE (user_email)
-)
-CREATE TABLE IF NOT EXISTS user_admin (
-	id INTEGER NOT NULL, 
-	user_id INTEGER, 
-	roles_id INTEGER, 
-	PRIMARY KEY (id), 
-	FOREIGN KEY(user_id) REFERENCES users (id)
-)
+);
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
 INSERT INTO `users` VALUES (1,'admin','admin','admin', 'admin@admin.com', '$2b$12$JbYx.GgrXWxZ0wIZxXbP/eyP7pG7A3cru.osarVqhv3KRUVkSqZxG');
-INSERT INTO `user_admin` VALUES (1,(users.id) 1)
+INSERT INTO `user_admin` VALUES (1,(users.id) 1);
 
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
