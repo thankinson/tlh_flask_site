e57f3338347a6699f9d0e325e3c7ea29f178147f
Tom <t_hankinson@outlook.com>
2022-10-04 15:25:48 +0100

    update

--- flask-app/application/forms/forms.py
+++ flask-app/application/forms/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, EqualTo, Email, ValidationError
+from wtforms.validators import DataRequired, Length, EqualTo, Email
 
 class UserRegistration(FlaskForm):
     user_name = StringField('User Name: ', validators=[DataRequired(), Length(min=4, max=30)])
