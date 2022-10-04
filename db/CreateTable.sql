1099f39491ce501cc0314f231ec782fdc5086414
Tom <t_hankinson@outlook.com>
2022-10-04 14:50:49 +0100

    update db create

--- db/CreateTable.sql
+++ db/CreateTable.sql
@@ -21,6 +21,6 @@ CREATE TABLE IF NOT EXISTS user_admin (
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
 INSERT INTO `users` VALUES (1,'admin','admin','admin', 'admin@admin.com', '$2b$12$JbYx.GgrXWxZ0wIZxXbP/eyP7pG7A3cru.osarVqhv3KRUVkSqZxG');
-INSERT INTO `user_admin` VALUES (1, 1)
+INSERT INTO `user_admin` VALUES (1,(users.id) 1)
 
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
