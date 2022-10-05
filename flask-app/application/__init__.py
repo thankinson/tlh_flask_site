fe650662454e5b043379a0c574fcfcb38e0063af
tom <t_hankinson@outlook.com>
2022-10-05 19:54:02 +0100

    update

--- flask-app/application/__init__.py
+++ flask-app/application/__init__.py
@@ -9,8 +9,8 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'bA5qzruPYLAyyx5QFNUVCg'
 # app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///data.db"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:password@database:3306/finalproject'
-
 db =SQLAlchemy(app)
+
 bcrypt = Bcrypt(app)        
 csrf.init_app(app)
 
