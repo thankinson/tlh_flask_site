3122ba223e03fdbb53c8670779d23a5c82d1c1e6
Tom <t_hankinson@outlook.com>
2022-10-06 13:40:37 +0100

    update

--- db-dump/init.sql
+++ db-dump/init.sql
@@ -9,14 +9,14 @@ CREATE TABLE IF NOT EXISTS `users` (
 	PRIMARY KEY(`id`),
 	UNIQUE(`user_email`),
 	UNIQUE(`user_name`)
-);
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 CREATE TABLE IF NOT EXISTS `user_admin` (
 	`id` INTEGER NOT NULL,
 	`user_id`	INTEGER,
 	`roles_id` INTEGER,
 	FOREIGN KEY(`user_id`) REFERENCES `users`(`id`),
 	PRIMARY KEY(`id`)
-);
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 INSERT INTO `users` (`id`,`user_name`,`first_name`,`last_name`,`user_email`,`password`,`remember_user`) VALUES (1,'admin','admin','admin','admin@email.com','$2b$12$WovjzWcD6y9XQtvocqiosOzwlp/xLrDLG7Nz.WaHpYoVJaLeGx8Om',0);
 INSERT INTO `user_admin` (`id`,`user_id`,`roles_id`) VALUES (1,1,1);
 
