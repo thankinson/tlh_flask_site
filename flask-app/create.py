0ceac17eaa2d2b81ac1d01ff9e0bd8cc35a2fc6c
Tom <t_hankinson@outlook.com>
2022-10-06 11:08:43 +0100

    update

--- flask-app/create.py
+++ flask-app/create.py
@@ -5,23 +5,22 @@ from application import bcrypt
 
 # default password for all suers is password
 
-def Dbbuild():
-    db.drop_all()
-    db.create_all()
-    hash_pw = bcrypt.generate_password_hash('password')
-    AdminUser = Users(
-        user_name='admin',
-        first_name='admin',
-        last_name='admin',
-        user_email='admin@email.com',
-        password= hash_pw
-        )
-    db.session.add(AdminUser)
+db.drop_all()
+db.create_all()
+hash_pw = bcrypt.generate_password_hash('password')
+AdminUser = Users(
+    user_name='admin',
+    first_name='admin',
+    last_name='admin',
+    user_email='admin@email.com',
+    password= hash_pw
+    )
+db.session.add(AdminUser)
 
-    addUserAdmin = UserAdmin(
-                        users=AdminUser,
-                        roles_id= 1)
-    db.session.add(addUserAdmin)
+addUserAdmin = UserAdmin(
+                    users=AdminUser,
+                    roles_id= 1)
+db.session.add(addUserAdmin)
 
 # # users
 # AdminUser = Users(
