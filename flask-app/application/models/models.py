565c646c80c780d5e8253fb4f85c0bca9b1af318
Tom <t_hankinson@outlook.com>
2022-10-07 16:57:48 +0100

    update

--- flask-app/application/models/models.py
+++ flask-app/application/models/models.py
@@ -1,6 +1,5 @@
 from application import db, login_manager
 from flask_login import UserMixin
-from sqlalchemy.dialects.mysql import INTEGER, CHAR, BOOLEAN
 
 class Users(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
@@ -9,18 +8,18 @@ class Users(db.Model, UserMixin):
     last_name = db.Column(db.String(30), nullable=False)
     user_email = db.Column(db.String(30), nullable=False, unique=True)
     password = db.Column(db.String(200), nullable=False)
-    remember_user = db.Column(db.Boolean, default=False)
+    # remember_user = db.Column(db.Boolean, default=False)
     admin_id = db.relationship('UserAdmin', backref='users')
 
-    def is_active(self):
-        return True
+    # def is_active(self):
+    #     return True
     
-    def get_id(self):
-        # return self.user_email
-        return self.id
+    # def get_id(self):
+    #     # return self.user_email
+    #     return self.id
     
-    def is_authenticated(self):
-        return self.remember_user
+    # def is_authenticated(self):
+    #     return self.remember_user
 
 @login_manager.user_loader
 def load_user(id):
